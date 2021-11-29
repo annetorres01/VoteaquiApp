95f92c1beb8c7d46cc84ffa324d61aa09aea746e
annetorres01 <annelouise1106@gmail.com>
2021-11-29 09:41:18 -0300

    Página home com o botão, lista e bottomNavegationView.

---
+++ app/src/main/java/com/example/voteaquiapp/CriaVotacaoActivity.java
@@ -0,0 +1,14 @@
+package com.example.voteaquiapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CriaVotacaoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cria_votacao);
+    }
+}
